537f51680134fe44dff03f325bfe558b349d9285
Anngu <Anngu@MSI>
2023-04-03 22:16:01 +0700

    Add project files.

---
+++ cs0/Vukhi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs0
+{
+    class Vukhi
+    {
+       public Vukhi()
+        {
+            Console.Write("Khoi tao");
+        }
+        public Vukhi(string a)
+        {
+            Console.Write("Khoi tao"+ a);
+        }
+
+
+    }
+}
